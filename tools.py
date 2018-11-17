c869877f6bc13d4aa10b0fea10aa5f99d183cd43
Cosmin-Petru Nechifor <cosmin.nechifor96@gmail.com>
2018-11-17 20:41:12 +0200

    adds user specific notes

--- tools.py
+++ tools.py
@@ -6,3 +6,17 @@ def get_token():
     token_path = os.path.join(path, 'token.txt')
     token = open(token_path, 'r').read().rstrip()
     return token
+
+
+def save_to_notes(msg):
+    first_name, last_name, message = msg
+    file_name = first_name + '_' + last_name + '.txt'
+    with open(file_name, 'a') as file:
+        file.write(message + '\n')
+
+
+def read_from_notes(user):
+    first_name, last_name = user
+    file_name = first_name + '_' + last_name + '.txt'
+    with open(file_name, 'r') as file:
+        return file.read()
